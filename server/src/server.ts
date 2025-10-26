b07abc2bd863a467d0325785636c96a357a23619
Mohammad Shahid <msa.ansari28@gmail.com>
2025-10-27 00:13:02 +0530

    `chore(app): setup Express server with routes, middleware, and DB connection`

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,20 +2,23 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
-import { Server } from 'socket.io';
 import connectDB from '@config/db';
+import taskRoutes from '@routes/taskRoutes';
+import userRoutes from '@routes/userRoutes';
 
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/tasks', taskRoutes);
+app.use('/api/users', userRoutes);
 
 
 const PORT = process.env.PORT || 5000;
