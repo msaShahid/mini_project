e55969720fa714ea53f78a5650de75949ccee901
Mohammad Shahid <msa.ansari28@gmail.com>
2025-10-28 16:43:11 +0530

    `feat(app): initialize application entry and routing setup - Configure RouterProvider and integrate AppRoutes with toast notifications - Wrap App component with AuthProvider  for global auth and navigation - Ensure consistent app-wide context and routing structure `

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// client/src/main.tsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import {AuthProvider} from "./context/AuthProvider.tsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <App />
+      <Toaster position="top-right" />
+    </AuthProvider>
+  </StrictMode>
+);
