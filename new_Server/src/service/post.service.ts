818545b2c691e364d08a628be7afb951155c0887
Mohammad Shahid <msa.ansari28@gmail.com>
2025-11-11 00:55:44 +0530

    `feat(post-service): implement post service layer with Mongoose operations - Added postCreate, postList, findPostByUserId, postUpdate, and postDelete functions - Used Mongoose model for database operations - Ensured strong typing between IPost and PostDTO - Improved postCreate method to automatically attach userId - Handled potential errors and missing documents gracefully `

---
+++ new_Server/src/service/post.service.ts
@@ -0,0 +1,30 @@
+import Post, {IPost} from '../models/post.model.js'
+
+const postService = {
+
+    async postList(): Promise<IPost[]> {
+        return await Post.find().sort({createdAt: -1});
+    },
+
+    async postFindByUserId(userId: string): Promise<IPost[]> {
+        return await Post.find({userId: userId}).sort({createdAt: -1});
+    },
+
+    async postFindById(id : string): Promise<IPost | null> {
+        return await Post.findById(id);
+    },
+
+    async postCreate(userId: string, data: IPost): Promise<IPost> {
+        return await Post.create({...data, userId});
+    },
+
+    async postUpdate(id: string, data: Partial<IPost>): Promise<IPost | null> {
+        return await Post.findByIdAndUpdate(id, data, {new : true});
+    },
+
+    async postDelete(id: string): Promise<IPost | null> {
+        return await Post.findByIdAndDelete(id);
+    } 
+}
+
+export default postService;
