ab549cdf25e300835092e8a6a1004a2cc2a5e171
Mohammad Shahid <msa.ansari28@gmail.com>
2025-11-11 00:58:34 +0530

    `feat: centralize app routing for easier route management`

--- new_Server/src/routes/index.ts
+++ new_Server/src/routes/index.ts
@@ -1,8 +1,10 @@
-import express from 'express'
-import userRouter from './user.routes.js'
+import express from 'express';
+import userRouter from './user.routes.js';
+import postUser from './post.routes.js';
 
 const router = express.Router();
 
 router.use('/user', userRouter);
+router.use('/post',postUser);
 
 export default router
