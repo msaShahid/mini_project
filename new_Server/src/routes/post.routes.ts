eb63fa1df33c3611119ea12a12b8e4dfbda257a4
Mohammad Shahid <msa.ansari28@gmail.com>
2025-11-22 22:24:26 +0530

    `feat(post-routes): posts route with auth and multiple image upload support`

--- new_Server/src/routes/post.routes.ts
+++ new_Server/src/routes/post.routes.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import postController from '../controller/post.controller.js'
 import authMiddleware from '../middleware/auth.middlewear.js';
+import { uploadPost } from '../middleware/uploadPost.js';
 
 const router = express.Router();
 
 router.get('/', postController.list);
 router.get('/user', authMiddleware, postController.userPost);
 router.get('/:id', authMiddleware, postController.getPostByid);
-router.post('/', authMiddleware, postController.create);
+router.post('/', authMiddleware, uploadPost.array('image',5), postController.create);
 router.put('/:id', authMiddleware, postController.update);
 router.delete('/:id', authMiddleware, postController.delete);
 
