31e132095ae9b8491e39e8c4bcaa59cc97568376
Mohammad Shahid <msa.ansari28@gmail.com>
2025-11-22 23:20:06 +0530

    `chore: setup Express in app.ts file with middleware, static folders, API routes, and global error handler`

---
+++ new_Server/src/app.ts
@@ -0,0 +1,29 @@
+import express, {Request, Response ,NextFunction } from 'express'
+import cors from 'cors';
+import path from 'path';
+import apiRouter from './routes/index.js';
+import { rateLimiter } from './middleware/rateLimiter.middleware.js';
+import { loggerMiddleware } from './middleware/logger.middleware.js';
+import { logger } from './utils/logger.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+app.use(loggerMiddleware);
+
+app.use('/api/v1', rateLimiter, apiRouter);
+
+// Global Error Handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(err.stack);
+    logger.error(err.message);
+    res.status(err.status || 500).json({
+        success: false,
+        message: err.message || 'Somthing went wrong.'
+    })
+})
+
+export default app;
