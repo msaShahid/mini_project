edadab2bae7ec07f624bbf8957608f2fee06f1b1
Mohammad Shahid <msa.ansari28@gmail.com>
2025-12-02 14:24:34 +0530

    `feat: apply validation middleware to /register endpoint using registerSchema`

--- new_Server/src/routes/user.routes.ts
+++ new_Server/src/routes/user.routes.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import userController from '../controller/user.controller.js';
 import { upload } from '../middleware/uploadSingle.js';
 import authProtected from '../middleware/auth.middlewear.js'
+import {registerSchema} from '../validations/user.validation.js'
+import {validate} from  '../middleware/validate.js'
 
 const router = express.Router();
 
-router.post('/register', userController.register);
+router.post('/register', validate(registerSchema), userController.register);
 router.post('/login', userController.login);
 
 router.get('/', userController.list);
