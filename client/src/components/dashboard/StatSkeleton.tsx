4777fb1c1d64e29cd603fd3a8e03edd1ee77fb71
Mohammad Shahid <msa.ansari28@gmail.com>
2025-12-19 23:16:30 +0530

    `feat: Add StatSkeleton component for loading placeholder of stats cards`

---
+++ client/src/components/dashboard/StatSkeleton.tsx
@@ -0,0 +1,8 @@
+const StatSkeleton = () => (
+  <div className="bg-white p-6 rounded-xl shadow-md animate-pulse">
+    <div className="h-4 w-32 bg-gray-200 rounded mb-4"></div>
+    <div className="h-8 w-16 bg-gray-300 rounded"></div>
+  </div>
+);
+
+export default StatSkeleton;
