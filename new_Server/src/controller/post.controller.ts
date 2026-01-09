274cb29869a2235bf3b2e0cc60e0113fa4e42339
Mohammad Shahid <msa.ansari28@gmail.com>
2026-01-10 00:43:39 +0530

    `feat(postController): broadcast newly created posts via WebSocket`

--- new_Server/src/controller/post.controller.ts
+++ new_Server/src/controller/post.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import postService from "../service/post.service.js";
 import mongoose from "mongoose";
+import { broadcast } from "../server.js";
 
 // Data transfer object for post creation/update 
 export interface PostDTO {
-  userId: mongoose.Types.ObjectId;  
+  userId: mongoose.Types.ObjectId;
   name: string;
   description: string;
   images?: string[];
@@ -15,121 +16,124 @@ export interface PostDTO {
 
 const postController = {
 
-    list: async (req: Request, res: Response, next: NextFunction) => {
-        try{
-            const data = await postService.postList();
-            res.status(200).json({
-                success: true,
-                message: 'Post List retrieved successfully.',
-                data,
-            })
-        }catch(error){
-            next(error)
-        }
-    },
-
-    userPost: async ( req: Request, res: Response, next: NextFunction) => {
-        try{
-            const userId = req.user?.id;
-            if(!userId) return res.status(401).json({message: 'User not authorized'})
-
-            const data = await postService.postFindByUserId(userId);
-            res.status(200).json({
-                success: true,
-                message: 'Posts retrieved for current user.',
-                data,
-            })
-        }catch(error){
-            next(error);
-        }
-    },
-
-    getPostByid : async (req: Request<{id : string}>, res: Response, next: NextFunction) => {
-        try{
-            const {id} = req.params;
-            const data = await postService.postFindById(id);
-
-            if(!data){
-                return res.status(404).json({
-                    success: false,
-                    message: 'Post not found.'
-                })
-            }
-
-            res.status(200).json({
-                success: true,
-                message: 'Post retrieved successfully.',
-                data
-            })
-        }catch(error){
-            next(error);
-        }
-    },
-
-    create: async (req: Request, res: Response, next: NextFunction) => {
-        try{
-            const userId = req.user?.id;
-            if(!userId) return res.status(401).json({message: 'User not authorized'})
-
-            const newData: PostDTO = req.body;
-            const data = await postService.postCreate(userId, newData);
-
-            res.status(201).json({
-                success: true,
-                message: 'Post Create Successfully.',
-                data,
-            })
-        }catch(error){
-            next(error)
-        }
-    },
-
-    update : async (req: Request<{id: string}>, res: Response, next: NextFunction) => {
-        try{
-            const {id} = req.params;
-            const newData: Partial<PostDTO> = req.body;
-
-            const data = await postService.postUpdate(id, newData);
-
-            if (!data) {
-                return res.status(404).json({
-                    success: false,
-                    message: 'Post not found.'
-                });
-            }
-
-            res.status(200).json({
-                success: true,
-                message: 'Post Update Successfully.',
-                data,
-            })
-
-        }catch(error){
-            next(error);
-        }
-    },
-
-    delete : async (req: Request<{id: string}>, res: Response, next: NextFunction) => {
-        try{
-            const {id} = req.params;
-
-            if(!id){
-                return res.status(401).json({
-                    success: false,
-                    message: 'Data not found.'
-                })
-            }
-
-            const data = await postService.postDelete(id);
-            res.status(200).json({
-                success: true,
-                message: 'Post delete successfully.',
-                data
-            })
-        }catch(error){
-            next(error);
-        }
+  list: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = await postService.postList();
+      res.status(200).json({
+        success: true,
+        message: 'Post List retrieved successfully.',
+        data,
+      })
+    } catch (error) {
+      next(error)
     }
+  },
+
+  userPost: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.user?.id;
+      if (!userId) return res.status(401).json({ message: 'User not authorized' })
+
+      const data = await postService.postFindByUserId(userId);
+      res.status(200).json({
+        success: true,
+        message: 'Posts retrieved for current user.',
+        data,
+      })
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  getPostByid: async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const data = await postService.postFindById(id);
+
+      if (!data) {
+        return res.status(404).json({
+          success: false,
+          message: 'Post not found.'
+        })
+      }
+
+      res.status(200).json({
+        success: true,
+        message: 'Post retrieved successfully.',
+        data
+      })
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  create: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId = req.user?.id;
+      if (!userId) return res.status(401).json({ message: 'User not authorized' })
+
+      const newData: PostDTO = req.body;
+      const data = await postService.postCreate(userId, newData);
+
+      res.status(201).json({
+        success: true,
+        message: 'Post Create Successfully.',
+        data,
+      })
+
+      broadcast({ type: "create", post: data });
+
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  update: async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const newData: Partial<PostDTO> = req.body;
+
+      const data = await postService.postUpdate(id, newData);
+
+      if (!data) {
+        return res.status(404).json({
+          success: false,
+          message: 'Post not found.'
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: 'Post Update Successfully.',
+        data,
+      })
+
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  delete: async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(401).json({
+          success: false,
+          message: 'Data not found.'
+        })
+      }
+
+      const data = await postService.postDelete(id);
+      res.status(200).json({
+        success: true,
+        message: 'Post delete successfully.',
+        data
+      })
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default postController;
